bb02c1b77ebe785b06a5899a707c05a55cec63ea
silohss <119551213+silohss@users.noreply.github.com>
2022-12-01 10:31:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,22 @@
-import pandas as pd
-import streamlit as st
-sheet_id = "178FW99M2zI-G27OYKkXwquAPmNhc5TQvT6PyV9N41Tk"
-sheet_name = "laksa"
-df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
-st.dataframe(df)
+# streamlit_app.py
 
+import streamlit as st
+from gsheetsdb import connect
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=0)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["https://docs.google.com/spreadsheets/d/178FW99M2zI-G27OYKkXwquAPmNhc5TQvT6PyV9N41Tk/edit?usp=sharing"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.A1} has a :{row.B2}:")
