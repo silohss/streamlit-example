e707615de58ab5b275636ec0bf8d0f321901f319
silohss <119551213+silohss@users.noreply.github.com>
2022-12-01 10:21:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 sheet_id = "178FW99M2zI-G27OYKkXwquAPmNhc5TQvT6PyV9N41Tk"
 sheet_name = "laksa"
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
